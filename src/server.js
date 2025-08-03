31ca2c6c2ad177cd408449d36d525de0adf6e494
Nikhil-2x <nikhilyadav101513@gmail.com>
2025-08-03 19:28:45 +0530

    add cron.js

--- src/server.js
+++ src/server.js
@@ -3,18 +3,23 @@ import dotenv from "dotenv";
 import { initDB } from "./config/db.js";
 import ratelimiter from "./middleware/rateLimiter.js";
 import transactionsRoute from "./routes/transactionsRoute.js";
+import job from "./config/cron.js";
 
 dotenv.config();
 
 const app = express();
+
+if (process.env.NODE_ENV === "production") {
+  job.start();
+}
 
 app.use(ratelimiter);
 app.use(express.json());
 
 const PORT = process.env.PORT || 5003;
 
-app.get("/health-check", (req, res) => {
-  res.send("Welcome to the Expense Tracker API");
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "OK" });
 });
 
 app.use("/api/transactions", transactionsRoute);
